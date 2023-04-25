2b72993f49de52116bb8a0bf748cdef93a90bb83
Fosiya <giamafosiya@gmail.com>
2023-04-25 16:36:14 +0200

    added createpost

---
+++ client/src/components/Createpost.js
@@ -0,0 +1,56 @@
+import {TextField, Button} from "@mui/material"
+import {useState} from "react"
+import axios from "axios"
+
+
+
+const Createpost = (props) => {
+
+    const [post, setPost] = useState("")
+
+
+const addPost = () => {
+    axios.post("http://localhost:4000/createpost", {
+
+        post: post
+    }).then((res) => {
+
+        setPost("")
+       console.log(res)
+       return axios.get("http://localhost:4000/getposts%22")
+       .then((res) => {
+        props.setPosts(res.data)
+       })
+
+
+
+    }).catch((err) => {
+        console.log(err)
+        if(err.response.status === 401) {
+            return alert("Login to create post")
+        }
+        alert(err.response.data[0].message)
+    })
+}
+    return (
+        <div className="w-full flex flex-col items-center">
+
+
+
+  <TextField 
+    onChange={e => setPost(e.target.value)}
+    value={post}
+
+
+  className="w-[200px] mt-2"
+  variant="outlined" label="Post"/>
+  <Button
+  onClick={addPost}
+  style={{backgroundColor: "#1877f2"}}
+  className=" text-white w-[200px] mt-2"
+  >Create Post</Button>
+        </div>
+    )
+}
+
+export default Createpost
