e028727dff50b13a325a989b9be3d087c9b7f14f
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-25 17:25:05 +0200

    removed unassacary lines

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,13 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+
 
 function MyNavbar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">DEVFlow</Navbar.Brand>
+        <Navbar.Brand href="/">DEVFlow</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -15,17 +15,7 @@ function MyNavbar() {
             <Nav.Link href="/Login">Login</Nav.Link>
             <Nav.Link href="/Register">Register</Nav.Link>
             <Nav.Link href="/Createpost">Create post</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+           
           </Nav>
         </Navbar.Collapse>
       </Container>
